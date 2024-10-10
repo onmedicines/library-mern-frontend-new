353586bec5d2eda25a04bccc9e966b12b764654e
onmedicines <semwalanurag26@gmail.com>
2024-10-11 00:37:03 +0530

    Conditionally render components based on the navlink clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,54 @@
+import { useState } from "react";
+import Home from "./pages/Home.jsx";
+import AddBook from "./pages/AddBook.jsx";
+import ViewBooks from "./pages/ViewBooks.jsx";
+
 export default function App() {
+  const [activeNavlink, setActiveNavlink] = useState("home");
+
+  function handleNavlinkChange(e) {
+    const { id } = e.target;
+    if (id === "home") {
+      setActiveNavlink("home");
+    } else if (id === "viewBooks") {
+      setActiveNavlink("viewBooks");
+    } else if (id === "addBook") {
+      setActiveNavlink("addBook");
+    }
+  }
+
+  function render() {
+    switch (activeNavlink) {
+      case "home":
+        return <Home />;
+      case "addBook":
+        return <AddBook />;
+      case "viewBooks":
+        return <ViewBooks />;
+      default:
+        return <ErrorPage />;
+    }
+  }
+
   return (
-    <div id="app-container">
+    <>
       <header id="header">
-        <nav>
+        <nav onClick={handleNavlinkChange}>
           <h1>Library</h1>
           <ul>
-            <li>Home</li>
-            <li>View Books</li>
+            <li id="home" className={activeNavlink === "home" ? "active" : "unactive"}>
+              Home
+            </li>
+            <li id="addBook" className={activeNavlink === "addBook" ? "active" : "unactive"}>
+              Add book
+            </li>
+            <li id="viewBooks" className={activeNavlink === "viewBooks" ? "active" : "unactive"}>
+              View book
+            </li>
           </ul>
         </nav>
       </header>
-      <main id="main">
-        <form>
-          <label htmlFor="bookName">
-            <span>Book:</span>
-            <input type="text" name="bookName" id="bookName" />
-          </label>
-          <label htmlFor="authorName">
-            <span>Author:</span>
-            <input type="text" name="authorName" id="authorName" />
-          </label>
-          <label htmlFor="numberOfPages">
-            <span>Pages:</span>
-            <input type="number" name="numberOfPages" id="numberOfPages" />
-          </label>
-        </form>
-      </main>
-    </div>
+      <main id="main">{render()}</main>
+    </>
   );
 }
