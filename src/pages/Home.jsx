950e6e4c8e220b691d806cd3e8efcb52a1a585bb
onmedicines <semwalanurag26@gmail.com>
2024-10-11 02:54:39 +0530

    Add home page and style it

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Library Web App (MERN Stack)</h1>
+      <p> The Library app is a dynamic web application built using the MERN stack (MongoDB, Express, React, Node.js). It offers a simple and intuitive interface with three main pages: a homepage, a form to add books, and a display page for viewing added books. Users can easily input details such as the book’s name, author, page count, a brief summary, and provide a rating in the form of stars. This platform streamlines book management, offering a clean and responsive design, ensuring an efficient user experience for book enthusiasts and organizers alike.</p>
+    </div>
+  );
+}
