3049a04e082d8740a12e6dfad8dccbd6c9fc1bf2
onmedicines <semwalanurag26@gmail.com>
2024-10-26 12:32:33 +0530

    Remove useless router-dom import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
